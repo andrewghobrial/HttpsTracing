cce572c8fd5f9d08862d654198ee8ff6ce8fc98b
Andrew Ghobrial <Andrew.Ghobrial@ibm.com>
2018-07-25 11:32:07 -0400

    Initial Commit

---
+++ HttpsTracing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HttpsTracing
+//
+//  Created by Andrew Ghobrial on 7/25/18.
+//  Copyright © 2018 Andrew Ghobrial. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HttpsTracing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HttpsTracing
+//
+//  Created by Andrew Ghobrial on 7/25/18.
+//  Copyright © 2018 Andrew Ghobrial. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
