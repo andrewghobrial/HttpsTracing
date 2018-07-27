c51820ecccfd5542603b8ca25f0167743c3380a5
Andrew Ghobrial <Andrew.Ghobrial@ibm.com>
2018-07-27 15:33:10 -0400

    added stack trace

--- HttpsTracing/ViewController.m
+++ HttpsTracing/ViewController.m
@@ -13,6 +13,24 @@
 @end
 
 @implementation ViewController
+- (IBAction)hmm:(id)sender {
+    NSLog(@"what's up andrew");
+    NSURL *url = [NSURL URLWithString: @"https://example.com/"];
+    NSURLSession *defaultSession = [NSURLSession sessionWithConfiguration: [NSURLSessionConfiguration ephemeralSessionConfiguration]];
+    NSURLSessionDataTask *dataTask = [defaultSession dataTaskWithURL: url
+                                                    completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                                                                if(error == nil)
+                                                                {
+//                                                                    NSString *string = [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
+//                                                                    NSLog(@"Data = %@",string);
+                                                                    NSLog(@"Data = %ld", [(NSHTTPURLResponse*) response statusCode]);
+                                                                }
+    
+                                                        }
+                                            ];
+    [dataTask  resume];
+    NSLog(@"%@",[NSThread callStackSymbols]);
+    }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
